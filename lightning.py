a0689dbf38693192c51559a01d3de66a7b1b2944
mickeyshoes <parody9c@gmail.com>
2022-06-11 23:45:43 +0900

    [ADD] checkpoint [MOD] data input dim

--- lightning.py
+++ lightning.py
@@ -4,18 +4,21 @@ import torch.optim as optim
 
 class LitLinear(pl.LightningModule):
 
-    def __init__(self, linear):
+    def __init__(self, model):
         super().__init__()
-        self.model = linear
+        self.model = model
 
     def training_step(self, batch, batch_idx):
         x,y = batch
+        print(x.shape, y.shape)
+        x = x.view(x.size(0), -1)
         y_hat = self.model(x)
         loss = F.mse_loss(y_hat,y)
         return loss
 
     def validation_step(self, batch, batch_idx):
         x,y = batch
+        x = x.view(x.size(0), -1)
         y_hat = self.model(x)
         loss = F.mse_loss(y_hat,y)
         self.log("Train loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
--- train.py
+++ train.py
@@ -1,12 +1,13 @@
 from unicodedata import name
 from lightning import LitLinear
 from models import LinearModel
-from albumentations.pytorch.transforms import ToTensorV2
+from torchvision import transforms
 import os
 from torchvision.datasets import MNIST
 from torch.utils.data import DataLoader, random_split
 import pytorch_lightning as pl
 import torch
+from pytorch_lightning.callbacks import ModelCheckpoint
 
 '''
 Reference
@@ -17,29 +18,40 @@ Reference
 
 def load_dataset(is_valid:bool):
     datasets= {}
-    dataset = MNIST(os.getcwd(), download=False, transform=ToTensorV2())
+    dataset = MNIST(os.getcwd(), train=True, download=True, transform=transforms.ToTensor())
     if is_valid:
-        train_dataset, val_dataset = random_split(dataset, [50000,5000])
+        train_dataset, val_dataset = random_split(dataset, [55000,5000])
         datasets['train'] = train_dataset
         datasets['val'] = val_dataset
         return datasets
     return dataset
 
-if name == "__main__":
+if __name__ == "__main__":
 
     #define Fields
     AVAIL_GPU = min(1, torch.cuda.device_count())
     BATCH_SIZE = 32
+    EPOCH = 20
     IS_VALID = True
     pl.seed_everything(8)
     
     datasets = load_dataset(IS_VALID)
-    train_dataloader = DataLoader(datasets['train'], batch_size=BATCH_SIZE)
-    if IS_VALID: 
-        val_datalodaer = DataLoader(datasets['val'], batch_size=BATCH_SIZE)
+    train_dataloader = DataLoader(datasets['train'], batch_size=BATCH_SIZE, num_workers=4)
+    val_datalodaer = ''
+    if IS_VALID:
+        val_datalodaer = DataLoader(datasets['val'], batch_size=BATCH_SIZE, num_workers=4)
 
 
     model = LitLinear(LinearModel())
     optimizer = model.configure_optimizers()
-    trainer = pl.Trainer()
+
+    checkpoint_callback = ModelCheckpoint(
+        filename='mnist-epoch{epoch:02d}-val_loss{val/loss:.2f}',
+        verbose=True,
+        save_last=True,
+        monitor='val_acc',
+        mode='max'
+    )
+
+    trainer = pl.Trainer(gpus=AVAIL_GPU, max_epochs=EPOCH, progress_bar_refresh_rate=20, callbacks=[checkpoint_callback])
     trainer.fit(model=model, train_dataloaders=train_dataloader)
